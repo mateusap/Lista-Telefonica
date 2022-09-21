63330def3014943c3062c4d8f32fa4bf6a7a517f
Mateus Silva <ma.aparecido.silva@hotmail.com>
2022-09-21 12:42:26 -0300

    Visual do windows form feito

--- Lista Telefonica/Form1.Designer.cs
+++ Lista Telefonica/Form1.Designer.cs
@@ -28,13 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.txtBoxLast = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtBoxContact = new System.Windows.Forms.TextBox();
+            this.lblContact = new System.Windows.Forms.Label();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtBoxAdress = new System.Windows.Forms.TextBox();
+            this.lblAdress = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtBoxSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblFooter = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(59, 39);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(47, 16);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nome:";
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(114, 38);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(140, 20);
+            this.txtBoxName.TabIndex = 1;
+            // 
+            // txtBoxLast
+            // 
+            this.txtBoxLast.Location = new System.Drawing.Point(114, 70);
+            this.txtBoxLast.Name = "txtBoxLast";
+            this.txtBoxLast.Size = new System.Drawing.Size(140, 20);
+            this.txtBoxLast.TabIndex = 3;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(25, 71);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(81, 16);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "Sobrenome:";
+            // 
+            // txtBoxContact
+            // 
+            this.txtBoxContact.Location = new System.Drawing.Point(114, 101);
+            this.txtBoxContact.Name = "txtBoxContact";
+            this.txtBoxContact.Size = new System.Drawing.Size(140, 20);
+            this.txtBoxContact.TabIndex = 5;
+            // 
+            // lblContact
+            // 
+            this.lblContact.AutoSize = true;
+            this.lblContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContact.Location = new System.Drawing.Point(50, 102);
+            this.lblContact.Name = "lblContact";
+            this.lblContact.Size = new System.Drawing.Size(56, 16);
+            this.lblContact.TabIndex = 4;
+            this.lblContact.Text = "Contato:";
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Location = new System.Drawing.Point(114, 129);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(140, 20);
+            this.txtBoxEmail.TabIndex = 7;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(58, 130);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(48, 16);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // txtBoxAdress
+            // 
+            this.txtBoxAdress.Location = new System.Drawing.Point(114, 159);
+            this.txtBoxAdress.Multiline = true;
+            this.txtBoxAdress.Name = "txtBoxAdress";
+            this.txtBoxAdress.Size = new System.Drawing.Size(140, 87);
+            this.txtBoxAdress.TabIndex = 9;
+            // 
+            // lblAdress
+            // 
+            this.lblAdress.AutoSize = true;
+            this.lblAdress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdress.Location = new System.Drawing.Point(39, 160);
+            this.lblAdress.Name = "lblAdress";
+            this.lblAdress.Size = new System.Drawing.Size(69, 16);
+            this.lblAdress.TabIndex = 8;
+            this.lblAdress.Text = "Endereço:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(53, 252);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(62, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Salvar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(121, 252);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(62, 23);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Apagar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(192, 252);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(62, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Limpar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxSearch
+            // 
+            this.txtBoxSearch.Location = new System.Drawing.Point(388, 38);
+            this.txtBoxSearch.Name = "txtBoxSearch";
+            this.txtBoxSearch.Size = new System.Drawing.Size(140, 20);
+            this.txtBoxSearch.TabIndex = 14;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.Location = new System.Drawing.Point(334, 39);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(48, 16);
+            this.lblSearch.TabIndex = 13;
+            this.lblSearch.Text = "Busca:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(307, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(269, 244);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // lblFooter
+            // 
+            this.lblFooter.AutoSize = true;
+            this.lblFooter.Location = new System.Drawing.Point(3, 315);
+            this.lblFooter.Name = "lblFooter";
+            this.lblFooter.Size = new System.Drawing.Size(157, 13);
+            this.lblFooter.TabIndex = 16;
+            this.lblFooter.Text = "Desenvolvido por: Mateus Silva";
+            this.lblFooter.Click += new System.EventHandler(this.lblFooter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(595, 328);
+            this.Controls.Add(this.lblFooter);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtBoxSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtBoxAdress);
+            this.Controls.Add(this.lblAdress);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtBoxContact);
+            this.Controls.Add(this.lblContact);
+            this.Controls.Add(this.txtBoxLast);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.lblName);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lista";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.TextBox txtBoxLast;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtBoxContact;
+        private System.Windows.Forms.Label lblContact;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtBoxAdress;
+        private System.Windows.Forms.Label lblAdress;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtBoxSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblFooter;
     }
 }
 
--- Lista Telefonica/Form1.cs
+++ Lista Telefonica/Form1.cs
@@ -16,5 +16,15 @@ namespace Lista_Telefonica
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblFooter_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
